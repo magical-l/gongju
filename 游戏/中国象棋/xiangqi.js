88ea7f260c599cf74aca68eadc379cb83b483f4a
梁文剑 <lwj621@163.com>
2025-09-19 17:38:27 +0800

    中国象棋：我微调默认全局规则

--- 游戏/中国象棋/xiangqi.js
+++ 游戏/中国象棋/xiangqi.js
@@ -1,5 +1,7 @@
 const 红方id = '红方';
 const 黑方id = '黑方';
+const 红方默认配置 = {name: 红方id, flag: 'red'};
+const 黑方默认配置 = {name: 黑方id, flag: 'black'};
 const 红方玩家id = 红方id + '玩家';
 const 黑方玩家id = 黑方id + '玩家';
 const 默认玩家顺序 = [红方玩家id, 黑方玩家id];
@@ -54,8 +56,8 @@ class 中国象棋 extends Game {
 			棋盘: cfg.棋盘,
 			battlefieldCfg: {rowSize: 10, colSize: 9},//现在是在‘棋局’里自行实现了_buildBattlefield。可以考虑凑父类的逻辑。
 			teams: {
-				[红方id]: {name: 红方id, flag: 'red', players: {[红方玩家id]: {name: 红方名字}}},//以后可以给玩家加技能，比如‘走两步’
-				[黑方id]: {name: 黑方id, flag: 'black', players: {[黑方玩家id]: {name: 黑方名字}}}
+				[红方id]: {...红方默认配置, players: {[红方玩家id]: {name: 红方名字}}},//以后可以给玩家加技能，比如‘走两步’
+				[黑方id]: {...黑方默认配置, players: {[黑方玩家id]: {name: 黑方名字}}}
 			},
 			unitTypes: Object.fromEntries(
 				Object.entries(cfg.棋子类型)
