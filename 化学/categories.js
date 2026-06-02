897830525b6d79a99cb0a8b57e670b4f592173a4
梁文剑 <lwj621@163.com>
2026-06-02 10:39:21 +0800

    +categories.js

---
+++ 化学/categories.js
@@ -0,0 +1,85 @@
+export default [
+	{
+		name: '金属',
+		symbol:'',
+		filterable: true,
+		children: [
+			{
+				name: '碱金属',
+				filterable: true
+			},
+			{
+				name: '碱土金属',
+				filterable: true
+			},
+			{
+				name: '过渡金属',
+				filterable: true
+			},
+			{
+				name: '贫金属',
+				filterable: true
+			},
+			{
+				name: '贵金属',
+				symbol:'💰', 
+				filterable: true
+			},
+		]
+	},
+	{
+		name: '类金属',
+		filterable: true
+	},
+	{
+		name: '活泼非金属',
+		filterable: true
+	},
+	{
+		name: '稀有气体',
+		filterable: true
+	},
+	{
+		name: '放射性',
+		symbol:'☢️',
+		filterable: true
+	},
+	{
+		name: '人造',
+		symbol:'🧪',
+		filterable: true
+	},
+	{
+		name: '稀土',
+		filterable: true
+	},
+	{
+		name: '铁磁性',
+		symbol: '🧲',
+		filterable: true
+	},
+	{
+		name: '卤素',
+		filterable: true
+	},
+	{
+		name: '物态',
+		children: [
+			{
+				name: '气态',
+				symbol:'🌀',
+				filterable: true
+			},
+			{
+				name: '液态',
+				symbol:'💧',
+				filterable: true
+			},
+			{
+				name: '固态',
+				symbol:'🪨',
+				filterable: true
+			},
+		]
+	}
+];
